fca38c70ccf09344cb6aaaf307ef1dc3c4536b09
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-07-08 01:58:35 -0700

    slicing and indexing in python for string

--- man.py
+++ man.py
@@ -1,32 +1,50 @@
-int_value=19
+# int_value=19
 
-string_value="string"
+# string_value="string"
 
-float_value=9.09
+# float_value=9.09
 
-complex_value=9+9j
+# complex_value=9+9j
 
-print(int_value)
-print(type(int_value))
+# print(int_value)
+# print(type(int_value))
 
-print(string_value)
-print(type(string_value))
+# print(string_value)
+# print(type(string_value))
 
-print(float_value)
-print(type(float_value))
+# print(float_value)
+# print(type(float_value))
 
-print(complex_value)
-print(type(complex_value))
+# print(complex_value)
+# print(type(complex_value))
 
-bool_exp=True
-print(type(bool_exp))
-print(bool_exp)
-comp=complex(1+5j)
-print(comp)
+# bool_exp=True
+# print(type(bool_exp))
+# print(bool_exp)
+# comp=complex(1+5j)
+# print(comp)
 
-str1="value"
-print(str1[0])
-print(str1[-1])
-print(str1[1:3])
-print(len(str1))
+# str1="value"
+# print(str1[0])
+# print(str1[-1])
+# print(str1[1:3])
+# print(len(str1))
+
+#استبدال الاindex
+
+# str1="hello"
+# str2="y" +str1[1:len(str1)]
+# print(str2)
+str3="mohammad"
+str4="Ha" + str3[2:len(str3)]
+print(str4)
+str5="Ha"+str3
+print(str5)
+str7="hello"
+str8=str7[0:2] +"mm"+str7[4:]
+print(str8)
+
+
+
+
 
